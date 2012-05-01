4c8943fb3360256539943f79f2c0b923085c0412
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-05-01 22:01:26 +0200

    First tranche

--- src/proto/core.clj
+++ src/proto/core.clj
@@ -1,6 +1,32 @@
-(ns proto.core)
+;; Clojure provides **protocols** and **data types** as tools to
+;; define *abstractions*.
+;;
+;; - Protocols provide high-performance polymorphic method dispatch
+;; - Data types provide a way to create concretions of abstractions
+;; defined either with protocols or Java interfaces.
+;;
+;; They are an idiomatic way to write abstractions and concretions
+;; without resorting to Java classes and interfaces.
+(ns proto.core
+  (:import [java.io FileInputStream InputStreamReader BufferedReader
+            FileOutputStream OutputStreamWriter BufferedWriter]))
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+(defn make-reader [src]
+  (-> src FileInputStream. InputStreamReader. BufferedReader.))
+
+(defn make-writer [dst]
+  (-> dst FileOutputStream. OutputStreamWriter. BufferedWriter.))
+
+(defn gulp [src]
+  (let [sb (StringBuilder.)]
+    (with-open [reader (make-reader src)]
+      (loop [c (.read reader)]
+        (if (neg? c)
+          (str sb)
+          (do
+            (.append sb (char c))
+            (recur (.read reader))))))))
+
+(defn expectorate [dst content]
+  (with-open [writer (make-writer dst)]
+    (.write writer (str content))))
